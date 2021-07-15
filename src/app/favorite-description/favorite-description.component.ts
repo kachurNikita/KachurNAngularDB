ad79070bbc3289348ad3d4235d8db4eec3d1382b
Kachur Nikita <ashtonkachur@icloud.com>
2021-07-15 14:59:57 +0300

    favorites page adeded

---
+++ src/app/favorite-description/favorite-description.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-favorite-description',
+  templateUrl: './favorite-description.component.html',
+  styleUrls: ['./favorite-description.component.scss']
+})
+export class FavoriteDescriptionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
